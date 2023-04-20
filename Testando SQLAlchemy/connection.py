71ad67b00d6791dae465415490a3a47655d49818
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-04-20 17:41:24 -0300

    Ainda não consegui utilizar o sqlalchemy de maneira simples

--- Testando SQLAlchemy/connection.py
+++ Testando SQLAlchemy/connection.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/FACULDADE')
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/BASEPESSOAL', echo=True)
 con = engine.connect()
-response = con.execute('SELECT * FROM academico;')
-for row in response:
-    print(row)
+cur = con.cursor
+
+con.execute("CREATE TABLE IF NOT EXISTS films (title text, director text, year text)")
+
+con.execute("INSERT INTO films (title, director, year) VALUES ('Doctor Strange', 'Scott Derrickson', '2016')")
+
+# Read
+result_set = con.execute("SELECT * FROM films")  
+for r in result_set:  
+    print(r)
